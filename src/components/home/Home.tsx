8237a200b2c8c4beee1cbb1316498070b61beb77
Kitsuya0828 <kitsuyaazuma@gmail.com>
2023-01-25 04:29:20 +0900

    [delete] unnecessary useEffect

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -2,28 +2,15 @@
 import { About } from "./About";
 import { Skills } from "./Skills";
 import { Experience } from "./Experience";
-import { useEffect, useState } from "react";
 import { FooterSocial } from "../common/FooterSocial";
 
 export const Home = () => {
-    const [isClient, setIsClient] = useState(false);
-    const home = () => {
-        if (isClient) {
-            return (
-                <>
-                    <About />
-                    <Skills />
-                    <Experience />
-                    <FooterSocial />
-                </>
-            );
-        } else {
-            return null;
-        }
-    };
-    useEffect(() => {
-        setIsClient(true);
-    }, []);
-
-    return <>{home()}</>;
+    return (
+        <>
+            <About />
+            <Skills />
+            <Experience />
+            <FooterSocial />
+        </>
+    );
 };
