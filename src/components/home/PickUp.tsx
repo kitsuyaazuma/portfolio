6b8af25e2befb9ec858d48bf1bf376084057d98f
Kitsuya0828 <kitsuyaazuma@gmail.com>
2024-03-10 19:27:35 +0900

    feat: update poster award

--- src/components/home/PickUp.tsx
+++ src/components/home/PickUp.tsx
@@ -24,8 +24,8 @@ export const PickUp = () => {
         justify="center"
         align="center"
       >
-        <div style={{ width: "350px" }}>
-          <Image p="lg" radius="md" src="imgs/Book.png" alt="Kitsuya Azuma"/>
+        <div style={{ width: "360px" }}>
+          <Image p="lg" radius="md" src="imgs/Book.png" alt="Book"/>
         </div>
         <Stack>
           <Text size="xl" fw={700}>『ReactではじめるChrome拡張開発入門』</Text>
@@ -50,7 +50,7 @@ export const PickUp = () => {
               highlightClassName={colorScheme === "dark" ? "highlight-dark" : "highlight-light"}
               searchWords={["1位を獲得"]}
               autoEscape={true}
-              textToHighlight={"ZennのTrending Books（2023/02）で1位を獲得した。"}
+              textToHighlight={"ZennのTrending Books（2023年2月）で1位を獲得した。2024年2月に改訂版を公開。"}
             />
           </Text>
           <Group spacing="sm" position="center" mt="xs">
@@ -70,7 +70,7 @@ export const PickUp = () => {
         align="center"
       >
         <div style={{ width: "360px" }}>
-          <Image p="lg" radius="md" src="imgs/CyberAgentTechBlog.png" alt="Kitsuya Azuma"/>
+          <Image p="lg" radius="md" src="imgs/CyberAgentTechBlog.png" alt="CyberAgentTechBlog"/>
         </div>
         <Stack>
           <Text size="xl" fw={700}>『クラウドにおけるコスト管理のためのリソースラベル監視ツールの開発』</Text>
@@ -101,6 +101,52 @@ export const PickUp = () => {
           <Group spacing="sm" position="center" mt="xs">
             <Button variant="light" color="green.9" size="md" component="a" href="https://developers.cyberagent.co.jp/blog/archives/43627/">
               ブログを読む
+            </Button>
+          </Group>
+        </Stack>
+      </Flex>
+
+
+      <Space h="xl" />
+
+      <Flex
+        direction={{ base: "column", sm: "row" }}
+        gap="lg"
+        justify="center"
+        align="center"
+      >
+        <div style={{ width: "360px" }}>
+          <Image p="lg" radius="md" src="imgs/IEICEPoster.png" alt="IEICEPoster"/>
+        </div>
+        <Stack>
+          <Text size="xl" fw={700}>『連合学習の通信量を削減するためのEnergyスコアを利用した知識蒸留手法の検討』</Text>
+          <Text size="sm">
+            <Highlighter
+              highlightClassName={colorScheme === "dark" ? "highlight-dark" : "highlight-light"}
+              searchWords={["IEICE", "学生ポスターセッション", "優秀ポスター賞"]}
+              autoEscape={true}
+              textToHighlight={"広島大学で開催されたIEICE（電子情報通信学会）総合大会2024の学生ポスターセッションで優秀ポスター賞（上位7%）を受賞。"}
+            />
+          </Text>
+          <Text size="sm">
+            <Highlighter
+              highlightClassName={colorScheme === "dark" ? "highlight-dark" : "highlight-light"}
+              searchWords={["連合学習", "分布外検出"]}
+              autoEscape={true}
+              textToHighlight={"蒸留ベースの連合学習手法において十分に学習されたデータに対する予測のみを集約するため、初めて分布外検出スコアを取り入れ、通信量の削減とともに学習の効率化に成功した。"}
+            />
+          </Text>
+          <Text size="sm">
+            <Highlighter
+              highlightClassName={colorScheme === "dark" ? "highlight-dark" : "highlight-light"}
+              searchWords={["卒業論文"]}
+              autoEscape={true}
+              textToHighlight={"発表内容をさらに発展させ、卒業論文として執筆中。"}
+            />
+          </Text>
+          <Group spacing="sm" position="center" mt="xs">
+            <Button variant="light" color="green.9" size="md" component="a" href="https://kitsuya0828.github.io/DSFLplus/">
+              プロジェクトページへ
             </Button>
           </Group>
         </Stack>
