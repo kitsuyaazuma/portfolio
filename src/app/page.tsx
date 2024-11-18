b575fa8e7e7f73ef692a626f87ff942161b02441
kitsuya0828 <kitsuyaazuma@gmail.com>
2024-11-18 23:09:43 +0900

    fix: use next Metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,11 @@
+import type { Metadata } from "next";
 import { App } from "@/components/App";
+
+export const metadata: Metadata = {
+  title: "Kitsuya Azuma's Portfolio",
+  description: "東桔也（あずまきつや）のポートフォリオサイト",
+  icons: ["/favicon_io/favicon.ico"],
+};
 
 export default function Home() {
   return (
