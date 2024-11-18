07dea4b68ffafe33fccbe4dc52557a0dac2701fb
kitsuya0828 <kitsuyaazuma@gmail.com>
2024-11-18 22:32:31 +0900

    fix: do not repeat experiences

---
+++ src/components/data/experience.ts
@@ -0,0 +1,145 @@
+export type ExperienceItem = {
+  title: string;
+  titleUrl: string;
+  details: string[];
+  skills: string[];
+  isCurrent: boolean;
+  category: ExperienceCategory;
+};
+
+export type ExperienceCategory = "Internship" | "Education" | "Award";
+
+export const experiences: ExperienceItem[] = [
+  {
+    title: "東京科学大学（旧：東京工業大学）",
+    titleUrl: "https://www.isct.ac.jp/ja",
+    details: ["2024年4月 -　工学院 情報通信系", "2024年4月 -　西尾研究室"],
+    skills: ["情報通信", "機械学習", "連合学習"],
+    isCurrent: true,
+    category: "Education",
+  },
+  {
+    title: "株式会社ミラティブ",
+    titleUrl: "https://www.mirrativ.co.jp/",
+    details: ["2024年2月 - 2024年6月　インターン（インフラエンジニア）"],
+    skills: ["Go", "Linux", "Fluentd", "Fluent Bit"],
+    isCurrent: false,
+    category: "Internship",
+  },
+  {
+    title: "エムスリー株式会社",
+    titleUrl: "https://corporate.m3.com/",
+    details: ["2024年3月　インターン（MLOpsエンジニア @AI・機械学習チーム）"],
+    skills: ["Go", "Kubernetes", "Google Cloud"],
+    isCurrent: false,
+    category: "Internship",
+  },
+  {
+    title: "東北大学",
+    titleUrl: "https://www.tohoku.ac.jp/japanese/",
+    details: [
+      "2020年4月 - 2024年3月　工学部電気情報物理工学科",
+      "2023年4月 - 2024年3月　大町研究室",
+    ],
+    skills: ["情報工学", "機械学習", "画像処理"],
+    isCurrent: false,
+    category: "Education",
+  },
+  {
+    title: "株式会社サイバーエージェント",
+    titleUrl: "https://www.cyberagent.co.jp/",
+    details: [
+      "2023年9月　次世代トップエンジニア創出インターンシップ ACE（最優秀チーム賞）",
+    ],
+    skills: ["TypeScript", "MySQL", "GraphQL", "AWS"],
+    isCurrent: false,
+    category: "Internship",
+  },
+  {
+    title: "ヤフー株式会社",
+    titleUrl: "https://about.yahoo.co.jp/",
+    details: [
+      "2023年8月　インターン（2023 Summer Internship クラウドインフラ開発 @クラウドプラットフォーム本部 ）",
+    ],
+    skills: ["Linux", "OpenStack", "Observability", "OpenObserve"],
+    isCurrent: false,
+    category: "Internship",
+  },
+  {
+    title: "株式会社サイバーエージェント",
+    titleUrl: "https://www.cyberagent.co.jp/",
+    details: ["2023年7月　インターン（CA Tech JOB @Abema Cloud Platform）"],
+    skills: ["Go", "Google Cloud", "AWS", "Terraform"],
+    isCurrent: false,
+    category: "Internship",
+  },
+  {
+    title: "Be Doctor Consulting株式会社",
+    titleUrl: "http://be-doctor.com/",
+    details: ["2022年11月 - 2023年4月　インターン（業務委託）"],
+    skills: ["React", "GraphQL", "AWS", "Terraform"],
+    isCurrent: false,
+    category: "Internship",
+  },
+  {
+    title: "JPHACKS 2022",
+    titleUrl: "https://jphacks.com/2022/result/",
+    details: [
+      "2022年10月 - 2022年11月　ハッカソン（イノベーター認定、スポンサー賞×3）",
+    ],
+    skills: ["React", "TypeScript", "Firebase"],
+    isCurrent: false,
+    category: "Award",
+  },
+  {
+    title: "株式会社Langsmith",
+    titleUrl: "https://corp.langsmith.co.jp/",
+    details: ["2022年4月 - 2023年4月　インターン（フロントエンドエンジニア）"],
+    skills: ["React", "TypeScript", "Next.js", "Tailwind CSS"],
+    isCurrent: false,
+    category: "Internship",
+  },
+  {
+    title: "学校法人角川ドワンゴ学園",
+    titleUrl: "https://nnn.ed.jp/course/net_commute/",
+    details: ["2021年4月 - 2022年11月　インターン（プログラミングTA）"],
+    skills: ["Premiere Pro", "Photoshop", "Illustrator", "JavaScript"],
+    isCurrent: false,
+    category: "Internship",
+  },
+  {
+    title: "JPHACKS 2021",
+    titleUrl: "https://jphacks.com/2021/result/",
+    details: [
+      "2021年10月 - 2021年11月　ハッカソン（審査員特別賞、イノベーター認定、スポンサー賞×5）",
+    ],
+    skills: ["Python", "機械学習"],
+    isCurrent: false,
+    category: "Award",
+  },
+  {
+    title: "楽天株式会社",
+    titleUrl: "https://corp.rakuten.co.jp/",
+    details: ["2022年8月　インターン（夏の陣2022 新規プロトタイプ開発コース）"],
+    skills: ["Ruby on Rails", "Ruby", "Docker"],
+    isCurrent: false,
+    category: "Internship",
+  },
+  {
+    title: "群馬県立前橋高等学校",
+    titleUrl: "https://maebashi-hs.gsn.ed.jp/",
+    details: ["2017年4月 - 2020年3月"],
+    skills: [],
+    isCurrent: false,
+    category: "Education",
+  },
+  {
+    title: "International Science School 2019 - The University of Sydney",
+    titleUrl:
+      "https://www.sydney.edu.au/science/industry-and-community/community-engagement/international-science-school.html",
+    details: ["2019年7月　オーストラリア科学奨学生"],
+    skills: [],
+    isCurrent: false,
+    category: "Education",
+  },
+];
--- src/components/home/Experience.tsx
+++ src/components/home/Experience.tsx
@@ -9,6 +9,22 @@ import {
   Container,
 } from "@mantine/core";
 import { TbDevicesPc, TbSchool, TbTrophyFilled } from "react-icons/tb";
+import { experiences, ExperienceCategory } from "../data/experience";
+
+const currentCount = experiences.filter((exp) => exp.isCurrent).length;
+
+const getBullet = (category: ExperienceCategory) => {
+  switch (category) {
+    case "Internship":
+      return <TbDevicesPc />;
+    case "Education":
+      return <TbSchool />;
+    case "Award":
+      return <TbTrophyFilled />;
+    default:
+      return <TbDevicesPc />;
+  }
+};
 
 export const Experience = () => {
   return (
@@ -17,463 +33,40 @@ export const Experience = () => {
         <Title>EXPERIENCE</Title>
       </Center>
       <Center>
-        <Timeline active={0} bulletSize={28} lineWidth={2} color="green.9">
-          <Timeline.Item
-            bullet={<TbSchool />}
-            title={
-              <Anchor
-                href="http://www.iic.ecei.tohoku.ac.jp/"
-                target="_blank"
-                color="green.9"
-              >
-                東京工業大学
-              </Anchor>
-            }
-          >
-            <Text size="xs" color="dimmed">
-              2024年4月 -　工学院 情報通信系
-            </Text>
-            <Text size="xs" color="dimmed">
-              2024年4月 -　
-              <Anchor
-                href="https://nishio-laboratory.github.io/"
-                target="_blank"
-                color="dimmed"
-              >
-                西尾研究室
-              </Anchor>
-            </Text>
-            <Group mt="sm" spacing="xs">
-              <Badge variant="light" size="sm" color="green.9">
-                情報通信
-              </Badge>
-              <Badge variant="light" size="sm" color="green.9">
-                機械学習
-              </Badge>
-              <Badge variant="light" size="sm" color="green.9">
-                連合学習
-              </Badge>
-            </Group>
-          </Timeline.Item>
-
-          <Timeline.Item
-            bullet={<TbDevicesPc />}
-            title={
-              <Anchor
-                href="https://www.mirrativ.co.jp/"
-                target="_blank"
-                color="dimmed"
-              >
-                株式会社ミラティブ
-              </Anchor>
-            }
-          >
-            <Text size="xs" color="dimmed">
-              2024年2月 - 2024年6月　インターン（インフラエンジニア）
-            </Text>
-            <Group mt="sm" spacing="xs">
-              <Badge variant="light" size="sm" color="gray.7">
-                Go
-              </Badge>
-              <Badge variant="light" size="sm" color="gray.7">
-                Linux
-              </Badge>
-              <Badge variant="light" size="sm" color="gray.7">
-                Fluentd
-              </Badge>
-              <Badge variant="light" size="sm" color="gray.7">
-                Fluent Bit
-              </Badge>
-            </Group>
-          </Timeline.Item>
-
-          <Timeline.Item
-            bullet={<TbDevicesPc />}
-            title={
-              <Anchor href="https://corporate.m3.com/" color="dimmed">
-                エムスリー株式会社
-              </Anchor>
-            }
-          >
-            <Text size="xs" color="dimmed">
-              2024年3月　インターン（MLOpsエンジニア @AI・機械学習チーム）
-            </Text>
-            <Group mt="sm" spacing="xs">
-              <Badge variant="light" size="sm" color="gray.7">
-                Go
-              </Badge>
-              <Badge variant="light" size="sm" color="gray.7">
-                Kubernetes
-              </Badge>
-              <Badge variant="light" size="sm" color="gray.7">
-                Google Cloud
-              </Badge>
-            </Group>
-          </Timeline.Item>
-
-          <Timeline.Item
-            bullet={<TbSchool />}
-            title={
-              <Anchor
-                href="https://www.tohoku.ac.jp/japanese/"
-                target="_blank"
-                color="dimmed"
-              >
-                東北大学
-              </Anchor>
-            }
-          >
-            <Text size="xs" color="dimmed">
-              2020年4月 - 2024年3月　工学部電気情報物理工学科
-            </Text>
-            <Text size="xs" color="dimmed">
-              2023年4月 - 2024年3月　
-              <Anchor
-                href="http://www.iic.ecei.tohoku.ac.jp/"
-                target="_blank"
-                color="dimmed"
-              >
-                大町研究室
-              </Anchor>
-            </Text>
-            <Group mt="sm" spacing="xs">
-              <Badge variant="light" size="sm" color="gray.7">
-                情報工学
-              </Badge>
-              <Badge variant="light" size="sm" color="gray.7">
-                機械学習
-              </Badge>
-              <Badge variant="light" size="sm" color="gray.7">
-                画像処理
-              </Badge>
-            </Group>
-          </Timeline.Item>
-
-          <Timeline.Item
-            bullet={<TbSchool />}
-            title={
-              <Anchor
-                href="https://toruyoshikawa.org/"
-                target="_blank"
-                color="dimmed"
-              >
-                吉川徹財団
-              </Anchor>
-            }
-          >
-            <Text size="xs" color="dimmed">
-              2023年4月 - 2024年3月　第1期奨学生
-            </Text>
-          </Timeline.Item>
-
-          <Timeline.Item
-            bullet={<TbDevicesPc />}
-            title={
-              <Anchor
-                href="https://www.cyberagent.co.jp/"
-                target="_blank"
-                color="dimmed"
-              >
-                株式会社サイバーエージェント
-              </Anchor>
-            }
-          >
-            <Text size="xs" color="dimmed">
-              2023年9月　次世代トップエンジニア創出インターンシップ
-              ACE（最優秀チーム賞）
-            </Text>
-            <Group mt="sm" spacing="xs">
-              <Badge variant="light" size="sm" color="gray.7">
-                TypeScript
-              </Badge>
-              <Badge variant="light" size="sm" color="gray.7">
-                MySQL
-              </Badge>
-              <Badge variant="light" size="sm" color="gray.7">
-                GraphQL
-              </Badge>
-              <Badge variant="light" size="sm" color="gray.7">
-                AWS
-              </Badge>
-            </Group>
-          </Timeline.Item>
-
-          <Timeline.Item
-            bullet={<TbDevicesPc />}
-            title={
-              <Anchor
-                href="https://about.yahoo.co.jp/"
-                target="_blank"
-                color="dimmed"
-              >
-                ヤフー株式会社
-              </Anchor>
-            }
-          >
-            <Text size="xs" color="dimmed">
-              2023年8月　インターン（2023 Summer Internship クラウドインフラ開発
-              @クラウドプラットフォーム本部 ）
-            </Text>
-            <Group mt="sm" spacing="xs">
-              <Badge variant="light" size="sm" color="gray.7">
-                Linux
-              </Badge>
-              <Badge variant="light" size="sm" color="gray.7">
-                OpenStack
-              </Badge>
-              <Badge variant="light" size="sm" color="gray.7">
-                Observability
-              </Badge>
-              <Badge variant="light" size="sm" color="gray.7">
-                OpenObserve
-              </Badge>
-            </Group>
-          </Timeline.Item>
-
-          <Timeline.Item
-            bullet={<TbDevicesPc />}
-            title={
-              <Anchor
-                href="https://www.cyberagent.co.jp/"
-                target="_blank"
-                color="dimmed"
-              >
-                株式会社サイバーエージェント
-              </Anchor>
-            }
-          >
-            <Text size="xs" color="dimmed">
-              2023年7月　インターン（CA Tech JOB @Abema Cloud Platform）
-            </Text>
-            <Group mt="sm" spacing="xs">
-              <Badge variant="light" size="sm" color="gray.7">
-                Go
-              </Badge>
-              <Badge variant="light" size="sm" color="gray.7">
-                Google Cloud
-              </Badge>
-              <Badge variant="light" size="sm" color="gray.7">
-                AWS
-              </Badge>
-              <Badge variant="light" size="sm" color="gray.7">
-                Terraform
-              </Badge>
-            </Group>
-          </Timeline.Item>
-
-          <Timeline.Item
-            bullet={<TbDevicesPc />}
-            title={
-              <Anchor
-                href="http://be-doctor.com/"
-                target="_blank"
-                color="dimmed"
-              >
-                Be Doctor Consulting株式会社
-              </Anchor>
-            }
-          >
-            <Text size="xs" color="dimmed">
-              2022年11月 - 2023年4月　インターン（業務委託）
-            </Text>
-            <Group mt="sm" spacing="xs">
-              <Badge variant="light" size="sm" color="gray.7">
-                React
-              </Badge>
-              <Badge variant="light" size="sm" color="gray.7">
-                GraphQL
-              </Badge>
-              <Badge variant="light" size="sm" color="gray.7">
-                AWS
-              </Badge>
-              <Badge variant="light" size="sm" color="gray.7">
-                Terraform
-              </Badge>
-            </Group>
-          </Timeline.Item>
-
-          <Timeline.Item
-            bullet={<TbTrophyFilled />}
-            title={
-              <Anchor
-                href="https://jphacks.com/2022/result/"
-                target="_blank"
-                color="dimmed"
-              >
-                JPHACKS 2022
-              </Anchor>
-            }
-          >
-            <Text size="xs" color="dimmed">
-              2022年10月 - 2022年11月
-              ハッカソン（イノベーター認定、スポンサー賞×3）
-            </Text>
-            <Group mt="sm" spacing="xs">
-              <Badge variant="light" size="sm" color="gray.7">
-                React
-              </Badge>
-              <Badge variant="light" size="sm" color="gray.7">
-                TypeScript
-              </Badge>
-              <Badge variant="light" size="sm" color="gray.7">
-                Firebase
-              </Badge>
-            </Group>
-          </Timeline.Item>
-
-          <Timeline.Item
-            bullet={<TbDevicesPc />}
-            title={
-              <Anchor
-                href="https://corp.langsmith.co.jp/"
-                target="_blank"
-                color="dimmed"
-              >
-                Langsmith株式会社
-              </Anchor>
-            }
-          >
-            <Text size="xs" color="dimmed">
-              2022年4月 - 2023年4月　インターン
-            </Text>
-            <Group mt="sm" spacing="xs">
-              <Badge variant="light" size="sm" color="gray.7">
-                Python
-              </Badge>
-              <Badge variant="light" size="sm" color="gray.7">
-                React
-              </Badge>
-              <Badge variant="light" size="sm" color="gray.7">
-                Google Cloud
-              </Badge>
-              <Badge variant="light" size="sm" color="gray.7">
-                Docker
-              </Badge>
-              <Badge variant="light" size="sm" color="gray.7">
-                Kubernetes
-              </Badge>
-            </Group>
-          </Timeline.Item>
-
-          <Timeline.Item
-            bullet={<TbDevicesPc />}
-            title={
-              <Anchor
-                href="https://nnn.ed.jp/course/net_commute/"
-                target="_blank"
-                color="dimmed"
-              >
-                学校法人角川ドワンゴ学園
-              </Anchor>
-            }
-          >
-            <Text size="xs" color="dimmed">
-              2021年4月 - 2022年11月 インターン（プログラミングTA）
-            </Text>
-            <Group mt="sm" spacing="xs">
-              <Badge variant="light" size="sm" color="gray.7">
-                Premiere Pro
-              </Badge>
-              <Badge variant="light" size="sm" color="gray.7">
-                Photoshop
-              </Badge>
-              <Badge variant="light" size="sm" color="gray.7">
-                Illustrator
-              </Badge>
-              <Badge variant="light" size="sm" color="gray.7">
-                JavaScript
-              </Badge>
-            </Group>
-          </Timeline.Item>
-
-          <Timeline.Item
-            bullet={<TbTrophyFilled />}
-            title={
-              <Anchor
-                href="https://jphacks.com/2021/result/"
-                target="_blank"
-                color="dimmed"
-              >
-                JPHACKS 2021
-              </Anchor>
-            }
-          >
-            <Text size="xs" color="dimmed">
-              2021年10月 - 2021年11月
-              ハッカソン（審査員特別賞、イノベーター認定、スポンサー賞×5）
-            </Text>
-            <Group mt="sm" spacing="xs">
-              <Badge variant="light" size="sm" color="gray.7">
-                Python
-              </Badge>
-              <Badge variant="light" size="sm" color="gray.7">
-                機械学習
-              </Badge>
-            </Group>
-          </Timeline.Item>
-
-          <Timeline.Item
-            bullet={<TbDevicesPc />}
-            title={
-              <Anchor
-                href="https://corp.rakuten.co.jp/"
-                target="_blank"
-                color="dimmed"
-              >
-                楽天株式会社
-              </Anchor>
-            }
-            lineVariant="dashed"
-          >
-            <Text size="xs" color="dimmed">
-              2022年8月 インターン（夏の陣2022 新規プロトタイプ開発コース）
-            </Text>
-            <Group mt="sm" spacing="xs">
-              <Badge variant="light" size="sm" color="gray.7">
-                Ruby on Rails
-              </Badge>
-              <Badge variant="light" size="sm" color="gray.7">
-                Ruby
-              </Badge>
-              <Badge variant="light" size="sm" color="gray.7">
-                Docker
-              </Badge>
-            </Group>
-          </Timeline.Item>
-
-          <Timeline.Item
-            bullet={<TbSchool />}
-            title={
-              <Anchor
-                href="https://maebashi-hs.gsn.ed.jp/"
-                target="_blank"
-                color="dimmed"
-              >
-                群馬県立前橋高等学校
-              </Anchor>
-            }
-          >
-            <Text size="xs" color="dimmed">
-              2017年4月 - 2020年3月
-            </Text>
-          </Timeline.Item>
-
-          <Timeline.Item
-            bullet={<TbSchool />}
-            title={
-              <Anchor
-                href="https://www.sydney.edu.au/science/industry-and-community/community-engagement/international-science-school.html"
-                target="_blank"
-                color="dimmed"
-              >
-                International Science School 2019 - The University of Sydney
-              </Anchor>
-            }
-          >
-            <Text size="xs" color="dimmed">
-              2019年7月 オーストラリア科学奨学生
-            </Text>
-          </Timeline.Item>
+        <Timeline
+          active={currentCount - 1}
+          bulletSize={28}
+          lineWidth={2}
+          color="green.9"
+        >
+          {experiences.map((exp, idx) => (
+            <Timeline.Item
+              bullet={getBullet(exp.category)}
+              title={
+                <Anchor
+                  href={exp.titleUrl}
+                  target="_blank"
+                  color={exp.isCurrent ? "green.9" : "dimmed"}
+                >
+                  {exp.title}
+                </Anchor>
+              }
+              key={idx}
+            >
+              {exp.details.map((detail) => (
+                <Text size="xs" color="dimmed" key={detail}>
+                  {detail}
+                </Text>
+              ))}
+              <Group mt="sm" spacing="xs">
+                {exp.skills.map((skill) => (
+                  <Badge variant="light" size="sm" color="gray.7" key={skill}>
+                    {skill}
+                  </Badge>
+                ))}
+              </Group>
+            </Timeline.Item>
+          ))}
         </Timeline>
       </Center>
     </Container>
