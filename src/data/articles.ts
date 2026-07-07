89da0c2027e01866b1881d8aeef2f580820b8983
kitsuyaazuma <kitsuyaazuma@gmail.com>
2026-07-08 06:31:39 +0900

    chore: update info
    Signed-off-by: kitsuyaazuma <kitsuyaazuma@gmail.com>

--- src/data/articles.ts
+++ src/data/articles.ts
@@ -2,6 +2,12 @@ import type { ArticleItem } from "../types/data";
 
 export const articleList: ArticleItem[] = [
   {
+    url: "https://www.geoffreylitt.com/2026/07/02/understanding-is-the-new-bottleneck.html",
+  },
+  {
     url: "https://www.anthropic.com/engineering/demystifying-evals-for-ai-agents",
+  },
+  {
+    url: "https://www.romaglushko.com/blog/k8s-gateway-api/",
   },
 ];
